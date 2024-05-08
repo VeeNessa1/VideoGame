4d84ed5131e91ea0294f77c03e80fbcbba5de64e
Vanessa Tapia <vtapia001@student.sdccd.edu>
2024-05-08 15:44:22 -0700

    Added the loading for the rest of the animations

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -12,17 +12,26 @@ import javax.swing.JPanel;
 
 import Inputs.KeyboardInputs;
 import Inputs.MouseInputs;
+import utilz.Constants;
 
 public class GamePanel extends JPanel{
 //We also need a constructor here
 	
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
-	private BufferedImage img;
 	private BufferedImage[][] animations; 	
 	private int aniTick, aniIndex, aniSpeed = 15;
 	//we need a speed, which will determine how fast we are going to animate, the lower the value, the faster it will go, fastest we can go is one 
 	//change x and y direction to float in order to slow down the rectangle
+	
+	private BufferedImage attack1_img;
+    private BufferedImage attack2_img;
+    private BufferedImage death_img;
+    private BufferedImage fall_img;
+    private BufferedImage hit_img;
+    private BufferedImage idle_img;
+    private BufferedImage jump_img;
+    private BufferedImage run_img;
 	
     
     public GamePanel() {
@@ -40,26 +49,61 @@ public class GamePanel extends JPanel{
 	
 	}
 	
-	private void loadAnimations() {
-		animations = new BufferedImage[1][6]; 
-		
-		for(int j = 0; j < animations.length; j++)
-		
-		for(int i = 0 ; i < animations[j].length; i++)
-			animations[j][i] = img.getSubimage(i * 231, j * 190, 231, 190);   
-		
-	}
+    private void loadAnimations() {
+    	final int ANIM_WIDTH = 231;
+    	final int ANIM_HEIGHT = 190;
 
+    	this.animations = new BufferedImage[8][8];
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.ATTACK_1); i++)
+    		this.animations[Constants.PlayerConstants.ATTACK_1][i] = this.attack1_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.ATTACK_2); i++)
+    		this.animations[Constants.PlayerConstants.ATTACK_2][i] = this.attack2_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.DEATH); i++)
+    		this.animations[Constants.PlayerConstants.DEATH][i] = this.death_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.FALLING); i++)
+    		this.animations[Constants.PlayerConstants.FALLING][i] = this.fall_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.HIT); i++)
+    		this.animations[Constants.PlayerConstants.HIT][i] = this.hit_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.IDLE); i++)
+    		this.animations[Constants.PlayerConstants.IDLE][i] = this.idle_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.JUMP); i++)
+    		this.animations[Constants.PlayerConstants.JUMP][i] = this.jump_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+
+    	for (int i = 0; i < Constants.PlayerConstants.GetSpriteAmount(Constants.PlayerConstants.RUNNING); i++)
+    		this.animations[Constants.PlayerConstants.RUNNING][i] = this.run_img.getSubimage(i * ANIM_WIDTH, 0, ANIM_WIDTH, ANIM_HEIGHT);
+	}
+    
 	private void importImg() {
-		InputStream is = getClass().getResourceAsStream("/Idle.png"); //tasking in an image as an input stream to load the image 
-		
-		try {
-			img = ImageIO.read(is);
-	} catch (IOException e) {
-		e.printStackTrace(); 
-		}
-	}
+        InputStream attack1_is = getClass().getResourceAsStream("/Attack1.png");
+        InputStream attack2_is = getClass().getResourceAsStream("/Attack2.png");
+        InputStream death_is = getClass().getResourceAsStream("/Death.png");
+        InputStream fall_is = getClass().getResourceAsStream("/Fall.png");
+        InputStream hit_is = getClass().getResourceAsStream("/Hit.png");
+        InputStream idle_is = getClass().getResourceAsStream("/Idle.png");
+        InputStream jump_is = getClass().getResourceAsStream("/Jump.png");
+        InputStream run_is = getClass().getResourceAsStream("/Run.png");
 
+        try {
+            this.attack1_img = ImageIO.read(attack1_is);
+            this.attack2_img = ImageIO.read(attack2_is);
+            this.death_img = ImageIO.read(death_is);
+            this.fall_img = ImageIO.read(fall_is);
+            this.hit_img = ImageIO.read(hit_is);
+            this.idle_img = ImageIO.read(idle_is);
+            this.jump_img = ImageIO.read(jump_is);
+            this.run_img = ImageIO.read(run_is);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+	
 	private void setPanelSize() {
 		Dimension size = new Dimension(1280,800);
 		setMinimumSize(size);

d9157bc871e566809064bd2aa011ff797f9ec11f
Vanessa Tapia <vtapia001@student.sdccd.edu>
2024-05-23 21:49:52 -0700

    add triva question & answer data

---
+++ src/main/Questions.java
@@ -0,0 +1,55 @@
+package main;
+
+public class Questions
+{
+  public static final String[] QUESTIONS = {
+    "Who is the main protagonist in \"The Lord of the Rings\"?",
+    "How many \"Star Wars\" movies have there been in total?",
+    "Which actor played the character Iron Man in the Marvel Cinematic Universe?",
+    "Who played the role of Hermione Granger in the Harry Potter film series?",
+    "Which film features the line \"May the Force Be With You\"?",
+    "Who is the main antagonist in \"The Lion King\"?",
+  };
+
+  public static final String[][] ANSWERS = {
+    {
+      "a) Frodo Baggins",
+      "b) Harry Potter",
+      "c) Tony Stark ",
+    },
+    {
+      "a) 6",
+      "b) 10",
+      "c) 11",
+    },
+    {
+      "a) Robert Downey Jr.",
+      "b) Chris Hemsworth",
+      "c) Chris Evans",
+    },
+    {
+      "a) Emma Roberts",
+      "b) Emma Stone",
+      "c) Emma Watson",
+    },
+    {
+      "a) Star Wars: Episode IV - A New Hope",
+      "b) Star Wars: Episode V - The Empire Strikes Back",
+      "c) Star Wars: Episode VI - Return of the Jedi",
+    },
+    {
+      "a) Scar",
+      "b) Simba",
+      "c) Rafiki",
+    },
+  };
+
+  public static final int[] ANSWER_INDICES = {
+    0,
+    2,
+    0,
+    2,
+    0,
+    0,
+  };
+}
